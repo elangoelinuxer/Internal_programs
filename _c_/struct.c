c1d4d523de1eb384d7325c8e2fbfc4a4bb4eb21c
Elango <ela.ece76@gmail.com>
2015-07-22 07:21:10 +0530

    test

---
+++ _c_/struct.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct college
+{
+
+int  rollno;
+float marks;
+char depot;
+
+};
+
+main()
+{
+
+
+struct college nandha;
+int a[5];
+
+printf("\nsize of struct is ...%d \n",sizeof(nandha));
+printf("\nsize of struct is ...%d \n",sizeof(main));
+
+
+}
